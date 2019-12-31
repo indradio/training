a290951ec45f34d9b0921b99c31d27d41462e3d6
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2019-12-31 11:11:26 +0700

    Update

--- application/views/pretest/selesai.php
+++ application/views/pretest/selesai.php
@@ -7,6 +7,12 @@
 											Selamat, Level anda <b><?= $pretest['level']; ?></b>.
 										</h4>
 										<P>
+										<?php if (!$pretest['soal_16'])
+										{
+											echo '<i>Anda telah menyelesaikan 15 Pertanyaan BASIC dan tidak perlu melanjutkan kepertanyaan berikutnya.</i>';
+										}
+										?>
+										<P>
                                         </br>Terima Kasih anda telah menyelesaikan pretest.
                                         </br>Kami akan segera menghubungi anda.
 									</div>
